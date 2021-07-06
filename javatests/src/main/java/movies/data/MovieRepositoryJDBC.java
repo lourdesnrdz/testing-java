92224be0ba1ffb36787ed9b8689e88087a8991dc
lourdesnrdz <lourdesnrdz@gmail.com>
2021-07-06 12:47:48 -0500

    creating bd and integration tests with db

---
+++ javatests/src/main/java/movies/data/MovieRepositoryJDBC.java
@@ -0,0 +1,41 @@
+package movies.data;
+
+import movies.model.Movie;
+
+import java.util.Collection;
+
+import movies.model.Genre;
+import movies.model.Movie;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+public class MovieRepositoryJDBC implements MovieRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public MovieRepositoryJDBC(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Movie findById(long id) {
+        return null;
+    }
+
+    @Override
+    public Collection<Movie> findAll() {
+        return jdbcTemplate.query("select * from movies", movieMapper);
+    }
+
+    @Override
+    public void saveOrUpdate(Movie movie) {
+
+    }
+
+    private static RowMapper<Movie> movieMapper = (rs, rowNum) ->
+            new Movie(
+                    rs.getInt("id"),
+                    rs.getString("name"),
+                    rs.getInt("minutes"),
+                    Genre.valueOf(rs.getString("genre")));
+}
