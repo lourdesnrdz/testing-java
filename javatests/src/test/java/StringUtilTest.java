b1ec6e4c0a569506c4d8d1ce64270448e25fbd0e
lourdesnrdz <lourdesnrdz@gmail.com>
2021-07-06 09:46:00 -0500

    organizing tests with JUnit

--- javatests/src/test/java/StringUtilTest.java
+++ javatests/src/test/java/StringUtilTest.java
@@ -5,8 +5,24 @@ import static org.junit.Assert.*;
 public class StringUtilTest {
 
     @Test
-    public void testRepeat() {
-        assertEquals("holaholahola", StringUtil.repeat("hola", 3));
+    public void repeat_string_once() {
         assertEquals("hola", StringUtil.repeat("hola", 1));
     }
+
+    @Test
+    public void repeat_string_multiple_times() {
+        assertEquals("holaholahola", StringUtil.repeat("hola", 3));
+    }
+
+    @Test
+    public void repeat_string_cero_times() {
+        assertEquals("", StringUtil.repeat("hola", 0));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void repeat_string_negative_times() {
+        StringUtil.repeat("hola", -1);
+    }
+
+
 }
