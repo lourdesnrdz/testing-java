52e97c94719677b7d4d45d4014cc0df703b3047d
lourdesnrdz <lourdesnrdz@gmail.com>
2021-07-06 10:25:09 -0500

    Challenge: create function isEmpty

---
+++ javatests/src/main/java/StringUtilChallenge.java
@@ -0,0 +1,11 @@
+public class StringUtilChallenge {
+
+    public static boolean isEmpty(String str) {
+
+        if(str == null || str.trim().length() <= 0) {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ javatests/src/test/java/StringUtilChallengeTest.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringUtilChallengeTest {
+
+    @Test
+    public void string_not_empty() {
+        assertFalse(StringUtilChallenge.isEmpty("hola"));
+    }
+
+    @Test
+    public void string_is_empty() {
+        assertTrue(StringUtilChallenge.isEmpty(""));
+    }
+
+    @Test
+    public void string_is_null() {
+        assertTrue(StringUtilChallenge.isEmpty(null));
+    }
+
+    @Test
+    public void string_with_spaces() {
+        assertTrue(StringUtilChallenge.isEmpty(" "));
+    }
+
+
+}
