84ca0011fa9d93e6801df59884e72f01e733656c
lourdesnrdz <lourdesnrdz@gmail.com>
2021-07-06 09:18:03 -0500

    unit testing with JUnit

--- javatests/src/test/java/StringUtilTest.java
+++ javatests/src/test/java/StringUtilTest.java
@@ -1,15 +1,12 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class StringUtilTest {
-    public static void main(String[] args) {
-        assertEquals(StringUtil.repeat("hola", 3), "holaholahola");
 
-        assertEquals(StringUtil.repeat("hola", 1), "hola");
-    }
-
-    private static void assertEquals(String actual, String expected){
-        if (!actual.equals(expected)){
-            throw new RuntimeException(actual + " is not equal to " + expected);
-        }
+    @Test
+    public void testRepeat() {
+        assertEquals("holaholahola", StringUtil.repeat("hola", 3));
+        assertEquals("hola", StringUtil.repeat("hola", 1));
     }
 }
